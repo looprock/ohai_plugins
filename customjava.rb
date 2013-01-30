4ad4338c3c2d044684123363b4dfa9e4c0e24350
Douglas Land <dsl@looprock.com>
2013-01-30 16:16:22 -0600

    more attempted fixes

--- customjava.rb
+++ customjava.rb
@@ -1,35 +1,36 @@
 provides "customjava"
 
 # detect non-provider versions of java on the system
+begin
+  customjava = Mash.new
 
-customjava = Mash.new
+  # start by assuming we don't have java installed
+  customjava[:result1] = false
 
-# start by assuming we don't have java installed
-customjava[:result] = false
-
-# see if there's a java binary in the path
-if system("which java  > /dev/null 2>&1") == true
-        customjava[:result] = true
-end
-# see if there are any procs running
-ps = `ps -C java|grep -v "PID TTY"|wc -l`.to_i
-if ps > 0
-  customjava[:result] = true
-end
-# see if there's anything under /usr/java
-javadirs = ["none"]
-if File.directory?("/usr/java") == true
-  if Dir.entries("/usr/java").empty? == false
-     customjava[:javadirs] = Dir.glob("/usr/java/*")
-     customjava[:result] = true
+  # see if there's a java binary in the path
+  if system("which java  > /dev/null 2>&1") == true
+        customjava[:result2] = true
   end
-end
-#customjava[:javadirs] = javadirs
+  # see if there are any procs running
+  ps = `ps -C java|grep -v "PID TTY"|wc -l`.to_i
+  if ps > 0
+    customjava[:result3] = true
+  end
+  # see if there's anything under /usr/java
+  customjava[:javadirs1] = ["none"]
+  if File.directory?("/usr/java") == true
+    if Dir.entries("/usr/java").empty? == false
+     customjava[:javadirs2] = Dir.glob("/usr/java/*")
+     customjava[:result4] = true
+    end
+  end
 
-# see if JAVA_HOME is set
-javahome = "none"
-if ENV['JAVA_HOME'] != nil
-        customjava[:result] = true
-        javahome = ENV['JAVA_HOME']
+  # see if JAVA_HOME is set
+  customjava[:java_home1] = "none"
+  if ENV['JAVA_HOME'] != nil
+        customjava[:result5] = true
+        customjava[:java_home2] = ENV['JAVA_HOME']
+  end
+rescue Exception => e
+  customjava[:result] = e.to_s
 end
-customjava[:java_home] = javahome
