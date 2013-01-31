063e7a14b4ca07773e97ef298a3a339a79fc379e
Douglas Land <dsl@looprock.com>
2013-01-30 23:02:02 -0600

    Charlie Sullivan is a rockstar and figured out my issue
    Should have been customjava Mash.new not customjava = Mash.new, the
    reset is all reverting from DEBUG mode.

--- customjava.rb
+++ customjava.rb
@@ -2,41 +2,41 @@ provides "customjava"
 
 # detect non-provider versions of java on the system
 begin
-  #customjava = Mash.new
+  customjava Mash.new
 
   # start by assuming we don't have java installed
-  #customjava[:result1] = false
-  customjava "do"
+  customjava[:result] = false
+  #customjava "do"
 
   # see if there's a java binary in the path
   if system("which java  > /dev/null 2>&1") == true
-        #customjava[:result2] = true
-        customjava "ray"
+        customjava[:result] = true
+        #customjava "ray"
   end
   # see if there are any procs running
   ps = `ps -C java|grep -v "PID TTY"|wc -l`.to_i
   if ps > 0
-    #customjava[:result3] = true
-    customjava "me"
+    customjava[:result] = true
+    #customjava "me"
   end
   # see if there's anything under /usr/java
-  #customjava[:javadirs1] = ["none"]
+  customjava[:javadirs] = ["none"]
   if File.directory?("/usr/java") == true
     if Dir.entries("/usr/java").empty? == false
-     #customjava[:javadirs2] = Dir.glob("/usr/java/*")
-     #customjava[:result4] = true
-     customjava "fa"
+     customjava[:javadirs] = Dir.glob("/usr/java/*")
+     customjava[:result] = true
+     #customjava "fa"
     end
   end
 
   # see if JAVA_HOME is set
-  #customjava[:java_home1] = "none"
+  customjava[:java_home] = "none"
   if ENV['JAVA_HOME'] != nil
-        #customjava[:result5] = true
-        #customjava[:java_home2] = ENV['JAVA_HOME']
-        customjava "so"
+        customjava[:result] = true
+        customjava[:java_home] = ENV['JAVA_HOME']
+        #customjava "so"
   end
 rescue Exception => e
-  #customjava[:result_err] = e.to_s
-  customjava "la"
+  customjava[:result] = e.to_s
+  #customjava "la"
 end
